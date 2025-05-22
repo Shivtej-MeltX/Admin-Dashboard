bb7ef5591c57f5de92b3f81a9403ede45e2ecc4a
Shivtej MeltX <shivtejpisal07.meltx@gmail.com>
2025-05-22 20:03:35 +0530

    Calendar Integrated

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,12 @@ import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
 import Invoices from "./scenes/invoices";
 import Form from "./scenes/form";
+import Calendar from "./scenes/calendar";
 // import Bar from "./scenes/bar";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
-// import Calender from "./scenes/calender";
 // import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -39,7 +39,7 @@ function App() {
               {/* <Route path="/pie" element={<Pie />} /> */}
               {/* <Route path="/faq" element={<FAQ />} /> */}
               {/* <Route path="/geography" element={<Geography />} /> */}
-              {/* <Route path="/calender" element={<Calender />} /> */}
+              <Route path="/calendar" element={<Calendar />} />
             </Routes>
           </main>
         </div>
