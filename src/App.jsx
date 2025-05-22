1105c8ce1884ddd3f530e71931e509c26f548d3f
Shivtej MeltX <shivtejpisal07.meltx@gmail.com>
2025-05-22 17:28:12 +0530

    Contacts

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ import Sidebar from "./scenes/global/Sidebar";
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
+import Contacts from "./scenes/contacts";
 // import Invoices from "./scenes/invoices";
 // import Bar from "./scenes/bar";
 // import Form from "./scenes/form";
-// import Contacts from "./scenes/contacts";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
@@ -31,7 +31,7 @@ function App() {
             <Routes>
               <Route path="/dashboard" element={<Dashboard />} />
               {/* <Route path="/invoices" element={<Invoices />} /> */}
-              {/* <Route path="/contacts" element={<Contacts />} /> */}
+              <Route path="/contacts" element={<Contacts />} />
               <Route path="/team" element={<Team />} />
               {/* <Route path="/bar" element={<Bar />} /> */}
               {/* <Route path="/form" element={<Form />} /> */}
