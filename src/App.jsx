28cd33611762950bf27fbabe606ffcd06662ac78
Shivtej MeltX <shivtejpisal07.meltx@gmail.com>
2025-05-22 17:45:57 +0530

    Invoices

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { Route, Routes } from "react-router-dom";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
-// import Invoices from "./scenes/invoices";
+import Invoices from "./scenes/invoices";
 // import Bar from "./scenes/bar";
 // import Form from "./scenes/form";
 // import Line from "./scenes/line";
@@ -30,7 +30,7 @@ function App() {
             <Topbar />
             <Routes>
               <Route path="/dashboard" element={<Dashboard />} />
-              {/* <Route path="/invoices" element={<Invoices />} /> */}
+              <Route path="/invoices" element={<Invoices />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/team" element={<Team />} />
               {/* <Route path="/bar" element={<Bar />} /> */}
